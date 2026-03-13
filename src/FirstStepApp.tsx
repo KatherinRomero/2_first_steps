567e4c540a6f3cc15ba7577656bab4fe1a9488c8
Kathe <katherinvanessa16@gmail.com>
2026-03-13 11:51:12 -0500

    Mostrar listados

--- src/FirstStepApp.tsx
+++ src/FirstStepApp.tsx
@@ -1,14 +1,29 @@
 import { ItemCounter } from "./shoping-cart/itemCounter";
 
+interface ItemInCar{
+producname:string,
+count:number
+}
+
+const ItemInCar:ItemInCar[]=[
+     {producname:"Nintendo Shich", count:2},
+     {producname:"iphone14", count:1},
+     {producname:"ipad", count:1},
+]
+
 
 export function FirstStepApp(){
    
     return(
         <>
-        <h1>Carrito de compras </h1>
-        <ItemCounter nameProducto="Nintendo Shich" count={2}/>
+        /*<h1>Carrito de compras </h1>
+        {ItemInCar.map(({producname,count})=>(
+            <ItemCounter key={producname} nameProducto={producname} count={count}/>
+        ))}
+       
+        {/*<ItemCounter nameProducto="Nintendo Shich" count={2}/>
         <ItemCounter nameProducto="iphone 15" count={2}/>
-        <ItemCounter nameProducto="iphone 12"count={0} />
+        <ItemCounter nameProducto="iphone 12"count={0} />*/}
         </>
     )
 }
