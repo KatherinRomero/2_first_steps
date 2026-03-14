24ca56326ce5ddee8287db16e3da72c948a57f59
Kathe <katherinvanessa16@gmail.com>
2026-03-14 16:27:03 -0500

    Eventos de los elemenstos

--- src/FirstStepApp.tsx
+++ src/FirstStepApp.tsx
@@ -1,14 +1,14 @@
 import { ItemCounter } from "./shoping-cart/itemCounter";
 
-interface ItemInCar{
+interface ItemInCar{// interfas para definir el tipo de objeto que se va a usar en el carrito de compras
 producname:string,
 count:number
 }
 
-const ItemInCar:ItemInCar[]=[
+const ItemInCar:ItemInCar[]=[// array de objetos que representan los productos en el carrito de compras
      {producname:"Nintendo Shich", count:2},
      {producname:"iphone14", count:1},
-     {producname:"ipad", count:1},
+     {producname:"ipad", count:0},
 ]
 
 
@@ -17,8 +17,8 @@ export function FirstStepApp(){
     return(
         <>
         /*<h1>Carrito de compras </h1>
-        {ItemInCar.map(({producname,count})=>(
-            <ItemCounter key={producname} nameProducto={producname} count={count}/>
+        {ItemInCar.map(({producname,count})=>(// map para iterar sobre el array
+            <ItemCounter key={producname} nameProducto={producname} count={count}/>//imprime el componente ItemCounter con las props 
         ))}
        
         {/*<ItemCounter nameProducto="Nintendo Shich" count={2}/>
--- src/shoping-cart/ItemCounter.tsx
+++ src/shoping-cart/ItemCounter.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react"
+
 interface Props{
     nameProducto:String,
-    count ?:number
+    
 }
 
 
 
-export const ItemCounter = ({nameProducto,count}:Props) => {
+export const ItemCounter = ({nameProducto,}:Props) => {
+const[quantity,setcount] =useState(0) // se está inicializando el estado quantity con el valor 10. La función setcount se utiliza para actualizar el valor de quantity cuando sea necesario.
+ 
+const handleAdd=()=>{//  Dentro de esta función, se utiliza setcount para actualizar el estado quantity incrementándolo en 1 cada vez que se hace clic.
+setcount(quantity+1)
+}
+
+const handlSumit=()=>{
+  if(quantity== 0)return//  Si quantity es igual a 1, la función retorna sin hacer nada, lo que evita que el contador baje de 1.
+setcount(quantity-1)
+}
+
+
+  const handleClick=()=>{
+  console.log(`click en ${nameProducto}`)
+}
   return (
     <section style={{
         display:'flex',/* display flex para alinear elementos horizontalmente */
@@ -16,9 +33,12 @@ export const ItemCounter = ({nameProducto,count}:Props) => {
       <span style={{
         width:65
       }}>{nameProducto}</span>
-      <button>+1</button>
-      <span>{count}</span> {/* La etiqueta span se usa para textos pequeños para editarlos mas fácilmente */}
-      <button>-1</button>
+      <button
+      onClick={handleAdd}
+      >+1</button>
+      <span>{quantity}</span> {/* La etiqueta span se usa para textos pequeños para editarlos mas fácilmente */}
+      <button onClick={handlSumit}
+      >-1</button>
     </section>
   )
 }
